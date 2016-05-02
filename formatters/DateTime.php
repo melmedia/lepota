2a0f64dd1601c6c6b84ebe429766a256a6a47acd
Mikhail Elfimov <melfimov@yopolis.ru>
2016-05-02 16:39:02 +0300

    PB-129 publication time format

--- formatters/DateTime.php
+++ formatters/DateTime.php
@@ -5,7 +5,8 @@ class DateTime
 {
     const DATE_TIME_SQL = 'Y-m-d H:i:sO';
     const DATE_SQL = 'Y-m-d';
-    
+    const DATE_TIME_ISO_8601 = 'Y-m-d\TH:i:sP';
+
     /**
      * Get date+time formatted for PostgreSQL timestamptz
      * @return string
@@ -27,6 +28,15 @@ class DateTime
             return $dateTime;
         }
         return $parts[0];
+    }
+
+    /**
+     * Convert SQL datetime format to AJAX format: "2016-01-30T14:55+03:00" (ISO-8601)
+     * @return string
+     */
+    public static function sqlToAjax($dateTime)
+    {
+        return str_replace(' ', 'T', $dateTime) . ':00';
     }
 
 }
