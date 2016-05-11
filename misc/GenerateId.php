6ab49619615850d90518cf9774fd3903f1f71246
Mikhail Elfimov <melfimov@movement51.com>
2016-05-11 21:06:51 +0300

    PB-126 upload image

---
+++ misc/GenerateId.php
@@ -0,0 +1,32 @@
+<?php
+namespace lepota\misc;
+
+/**
+ * Generate random text identifier
+ */
+class GenerateId
+{
+    /** Letters to generate ID from */
+    const LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /** Letters to generate ID from */
+    const DIGITS = "1234567890";
+
+    public static function letters($length)
+    {
+        return self::randomSymbols(self::LETTERS, $length);
+    }
+
+    public static function lettersAndNumbers($length)
+    {
+        return self::randomSymbols(self::LETTERS . self::DIGITS, $length);
+    }
+
+    protected static function randomSymbols($symbols, $length)
+    {
+        if ($length > strlen($symbols)) {
+            $symbols = str_repeat($symbols, (int) ceil($length / strlen($symbols)));
+        }
+        return substr(str_shuffle($symbols), 0, $length);
+    }
+
+}
