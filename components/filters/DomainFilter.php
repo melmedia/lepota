b5c7570f7a6cae670bc6f952f32674b7f9af75cf
Mikhail Elfimov <melfimov@movement51.com>
2016-05-17 18:39:23 +0300

    PB-126 preview background processing

---
+++ components/filters/DomainFilter.php
@@ -0,0 +1,37 @@
+<?php
+namespace lepota\components\filters;
+
+use Yii;
+use yii\base\ActionEvent;
+use yii\base\Behavior;
+use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
+
+class DomainFilter extends Behavior
+{
+    /** @var string */
+    public $deny;
+
+    /**
+     * Declares event handlers for the [[owner]]'s events.
+     * @return array events (array keys) and the corresponding event handler methods (array values).
+     */
+    public function events()
+    {
+        return [Controller::EVENT_BEFORE_ACTION => 'beforeAction'];
+    }
+
+    /**
+     * @param ActionEvent $event
+     * @return boolean
+     */
+    public function beforeAction($event)
+    {
+        if (strpos(Yii::$app->request->hostInfo, 'http://' . $this->deny) !== false) {
+            $event->isValid = false;
+            throw new ForbiddenHttpException;
+        }
+
+        return $event->isValid;
+    }
+}
