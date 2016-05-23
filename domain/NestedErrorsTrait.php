8ca3f7e6a25b81159af79bfd9e5dd0427b5fab5a
Mikhail Elfimov <melfimov@movement51.com>
2016-05-23 19:57:37 +0300

    PB-165 main page tiles

---
+++ domain/NestedErrorsTrait.php
@@ -0,0 +1,34 @@
+<?php
+namespace lepota\domain;
+
+/**
+ * Errors for nested objects stored as object (not array):
+ * {
+ *   "code": "EntityValidation",
+ *   "validationErrors": {
+ *     "mainPageTiles": {
+ *       "activeType": ["Must be one of values of list \"type\" attribute"]
+ *     }
+ *   }
+ * }
+ */
+trait NestedErrorsTrait
+{
+    protected $nestedErrors;
+
+    /**
+     * Adds a new error to the specified nested object
+     * @param string $envelope attribute name of nested object
+     * @param string $error new error message
+     */
+    public function setNestedErrors($envelope, $error)
+    {
+        $this->nestedErrors[$envelope] = $error;
+    }
+
+    public function getErrors($attribute = null)
+    {
+        return parent::getErrors($attribute) + $this->nestedErrors;
+    }
+
+}
--- exceptions/EntityValidationException.php
+++ exceptions/EntityValidationException.php
@@ -1,6 +1,8 @@
 <?php
 namespace lepota\exceptions;
 
+use Functional;
+use Yii;
 use yii\base\Model;
 
 /**
@@ -26,6 +28,49 @@ class EntityValidationException extends AjaxException
         else {
             $this->modelErrors = $modelErrors;
         }
+    }
+
+    /**
+     * Shortcut to create validation exception with one attribute is required
+     * @param string $paramName
+     * @return EntityValidationException
+     */
+    public static function paramRequired($paramName)
+    {
+        return new self([
+            $paramName => [Yii::t('yii', '{attribute} cannot be blank.', ['attribute' => $paramName])]
+        ]);
+    }
+
+    /**
+     * Shortcut to create validation exception with one attribute of object is required
+     * @param string $envelope
+     * @param string $paramName
+     * @return EntityValidationException
+     */
+    public static function nestedParamRequired($envelope, $paramName)
+    {
+        return new self([
+            $envelope => [
+                $paramName => [Yii::t('yii', '{attribute} cannot be blank.', ['attribute' => $paramName])]
+            ]
+        ]);
+    }
+
+    /**
+     * Shortcut to create validation exception with several attributes required
+     * @param string[] $params
+     * @return EntityValidationException
+     */
+    public static function paramsAreRequired($params)
+    {
+        $errors = array_combine(
+            $params,
+            Functional\map($params, function ($paramName) {
+                return [Yii::t('yii', '{attribute} cannot be blank.', ['attribute' => $paramName])];
+            })
+        );
+        return new self($errors);
     }
 
     public function getAjaxResponse($message = null)
