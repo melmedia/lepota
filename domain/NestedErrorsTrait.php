1f09133c7698dd1139a05aefbc71e543741dd26e
Mikhail Elfimov <melfimov@movement51.com>
2016-08-02 21:36:03 +0300

    PB-637 fix duplicate publication address

--- domain/NestedErrorsTrait.php
+++ domain/NestedErrorsTrait.php
@@ -31,4 +31,14 @@ trait NestedErrorsTrait
         return parent::getErrors($attribute) + $this->nestedErrors;
     }
 
+    /**
+     * Returns a value indicating whether there is any validation error.
+     * @param string|null $attribute attribute name. Use null to check all attributes.
+     * @return boolean whether there is any error.
+     */
+    public function hasErrors($attribute = null)
+    {
+        return parent::hasErrors($attribute) || ($attribute === null ? !empty($this->nestedErrors) : isset($this->nestedErrors[$attribute]));
+    }
+
 }
