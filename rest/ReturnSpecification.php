9b9cab10d85a3d41dbb2d35b9521b1b3556ef285
Mikhail Elfimov <melfimov@movement51.com>
2016-09-27 16:20:12 +0300

    PB-1092 rest return specification

---
+++ rest/ReturnSpecification.php
@@ -0,0 +1,46 @@
+<?php
+namespace lepota\rest;
+
+/**
+ * Allow to specify desired format of REST endpoint:
+ * return=only:count - limit response to only count of objects instead of objects list.
+ * return=addition:field1,field2,... - comma-separated list of additional fields to return in list.
+ */
+class ReturnSpecification
+{
+    const MODE_ONLY = 'only';
+    const MODE_ADDITION = 'addition';
+
+    public $mode;
+    public $only;
+    public $additions = [];
+
+    public function __construct(string $return = null)
+    {
+        if ($return) {
+            list($this->mode, $parts) = explode(':', $return);
+            $parts = explode(',', $parts);
+
+            switch ($this->mode) {
+                case self::MODE_ONLY:
+                    $this->only = $parts[0];
+                    break;
+
+                case self::MODE_ADDITION:
+                    $this->additions = $parts;
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Check if addition requested
+     * @param string $addition
+     * @return bool
+     */
+    public function addition(string $addition): bool
+    {
+        return in_array($addition, $this->additions);
+    }
+
+}
