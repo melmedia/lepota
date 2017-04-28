7364f7f419c4eb8f51774617b0b16147b84a561d
Mikhail Elfimov <melfimov@changers.team>
2017-04-28 13:21:56 +0300

    GRT-50 move common code from pablo to lepota

--- components/AjaxController.php
+++ components/AjaxController.php
@@ -7,6 +7,22 @@ use lepota\exceptions\AjaxException;
 class AjaxController extends \yii\rest\Controller
 {
 
+    public function behaviors()
+    {
+        return Functional\map(
+            Functional\reject(parent::behaviors(), function($filter, $index) {
+                return 'authenticator' == $index || 'rateLimiter' == $index;
+            }),
+            function ($filter, $f) {
+                if ('contentNegotiator' == $f) {
+                    $filter['formats'] = [
+                        'application/json' => Response::FORMAT_JSON,
+                    ];
+                }
+                return $filter;
+            });
+    }
+
     /**
      * Render AJAX exception
      * @param AjaxException $e
